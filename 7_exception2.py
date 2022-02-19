3061d06ba37e91733f10814af9bcd291e37729c8
Sergey Rytov <komatozz89@yandex.ru>
2022-02-19 10:35:17 +0300

    Исключения value

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,12 +13,27 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+def discounted(price, discount, max_discount=20):
+  try:  
+    if(not type(price) is int or not type(discount) is int or not type(max_discount) is int):
+      raise TypeError('Должны быть числами')
+    else:
+      price = abs(int(price))
+      discount = abs(int(discount))
+      max_discount = abs(float(max_discount))
+    if max_discount >= 100:
+      raise ValueError('Слишком большая максимальная скидка')
+    if discount >= max_discount:
+      return price
+    else:
+      return price - (price * discount / 100)
+
+  except  TypeError:
+    print('Должны быть числа')
+
+  except ValueError as big_discount:
+    print('Большая скидка')
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
@@ -26,3 +41,4 @@ if __name__ == "__main__":
     print(discounted("five", 5))
     print(discounted("сто", "десять"))
     print(discounted(100.0, 5, "10"))
+    print(discounted(100, 200, 200))
